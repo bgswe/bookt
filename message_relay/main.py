9229fb6a4b84287a57ecc8f8071f0c31848af2fb
bgswe <bg.soft.eng@gmail.com>
2024-01-15 11:47:29 -0700

    BOOK-16: update MR to utilize connection pools

--- message_relay/main.py
+++ message_relay/main.py
@@ -2,9 +2,9 @@ import logging
 import socket
 from time import sleep
 
-import psycopg2
 import structlog
 from confluent_kafka import Producer
+from psycopg_pool import ConnectionPool
 
 from message_relay.settings import (
     DATABASE_HOST,
@@ -36,85 +36,72 @@ def main():
 
     pending_messages = set()
 
-    def acked(err, msg):
-        log.info("kafka acked invoked")
+    with ConnectionPool(min_size=2) as pool:
 
-        key = str(msg.key(), encoding="utf-8")
+        def acked(err, msg):
+            if err is not None:
+                log = log.bind(message=str(msg), error=str(err))
+                log.error("failed to deliver message")
+            else:
+                key = str(msg.key(), encoding="utf-8")
 
-        if err is not None:
-            log = log.bind(message=str(msg), error=str(err))
-            log.error("failed to deliver message")
-        else:
-            conn = psycopg2.connect(
-                host=DATABASE_HOST,
-                dbname=DATABASE_NAME,
-                user=DATABASE_USER,
-                password=DATABASE_PASSWORD,
-            )
-
-            with conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(
-                        f"""
-                        DELETE FROM
-                            message_outbox
-                        WHERE
-                            id = '{key}'
-                    """,
-                    )
-
-                    pending_messages.remove(key)
-
-    while True:
-        log.info("Message Relay Loop")
-
-        conn = psycopg2.connect(
-            host=DATABASE_HOST,
-            dbname=DATABASE_NAME,
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD,
-        )
-
-        with conn:
-            with conn.cursor() as cursor:
-                outbox_query = f"""
-                    SELECT
-                        *
-                    FROM
-                        message_outbox
-                """
-
-                if len(pending_messages) > 0:
-                    outbox_query = (
-                        f"{outbox_query} WHERE id not in ({','.join(pending_messages)})"
-                    )
-
-                logger.debug(outbox_query)
-
-                cursor.execute(outbox_query)
-                columns = {desc[0]: i for i, desc in enumerate(cursor.description)}
-                messages = cursor.fetchall()
-
-                if messages:
-                    producer = Producer(conf)
-
-                    for m in messages:
-                        message_id = m[columns["id"]]
-
-                        producer.produce(
-                            "messages",
-                            key=message_id,
-                            value=bytes(m[columns["message"]]),
-                            callback=acked,
+                with pool.connection() as conn:
+                    with conn.cursor() as cursor:
+                        cursor.execute(
+                            f"""
+                            DELETE FROM
+                                message_outbox
+                            WHERE
+                                id = '{key}'
+                        """,
                         )
 
-                        pending_messages.add(message_id)
+                        pending_messages.remove(key)
 
-                    producer.poll(0.1)
-                else:
-                    logger.debug("there are no messages to push")
+        while True:
+            logger.info("--- MESSAGE RELAY ITERATION ---")
 
-                sleep(0.1)
+            with pool.connection() as conn:
+                with conn.cursor() as cursor:
+                    outbox_query = f"""
+                        SELECT
+                            *
+                        FROM
+                            message_outbox
+                    """
+
+                    # prevent querying for messages that are being processed
+                    if len(pending_messages) > 0:
+                        outbox_query = f"{outbox_query} WHERE id not in ({','.join(pending_messages)})"
+
+                    logger.debug(outbox_query)
+
+                    # query for messages, and map column names to returned column indexes
+                    cursor.execute(outbox_query)
+                    messages = cursor.fetchall()
+
+                    if messages:
+                        columns = {
+                            desc[0]: i for i, desc in enumerate(cursor.description)
+                        }
+                        producer = Producer(conf)
+
+                        for m in messages:
+                            message_id = m[columns["id"]]
+                            producer.produce(
+                                "messages",
+                                key=message_id,
+                                value=bytes(m[columns["message"]]),
+                                callback=acked,
+                            )
+
+                            pending_messages.add(message_id)
+
+                        producer.poll(1)
+                    else:
+                        logger.debug("there are no messages to push")
+
+                    sleep(1)
 
 
 if __name__ == "__main__":
